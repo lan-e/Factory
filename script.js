0c9bc3e85bb4651539fd51162eabcd45717a114a
lan-e <neladuranec@gmail.com>
2023-02-05 22:55:32 +0100

    have to fix bottom wrapper

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ $(document).ready(function () {
   let widthToScroll = imageWrapper.children[0].offsetWidth;
   let column = Math.floor(imageWrapper.offsetWidth / widthToScroll);
 
+  let imageWrapperBottom = document.querySelector(".image-wrapper-bottom");
+  let imageWrapperBottomChildren = Array.from(imageWrapperBottom.children);
+  let widthToScrollBottom = imageWrapperBottom.children[0].offsetWidth;
+  let columnBottom = Math.floor(
+    imageWrapperBottom.offsetWidth / widthToScrollBottom
+  );
+
   imageWrapperChildren.slice(0, column).forEach((item) => {
     imageWrapper.insertAdjacentHTML("beforeend", item.outerHTML);
   });
@@ -18,14 +25,7 @@ $(document).ready(function () {
   $(".image-wrapper").scrollLeft(imageWrapper.offsetWidth);
   $(".image-wrapper").removeClass("no-smooth");
 
-  $(".prev-arrow").on("click", function () {
-    imageWrapper.scrollLeft -= widthToScroll;
-  });
-
-  $(".next-arrow").on("click", function () {
-    imageWrapper.scrollLeft += widthToScroll;
-  });
-
+  //needed for looping
   $(".image-wrapper").on("scroll", function () {
     if (imageWrapper.scrollLeft === 0) {
       imageWrapper.classList.add("no-smooth");
@@ -41,10 +41,45 @@ $(document).ready(function () {
       imageWrapper.classList.remove("no-smooth");
     }
   });
+  //-------------------------------------------------------
 
+  imageWrapperBottomChildren.slice(0, columnBottom).forEach((item) => {
+    imageWrapperBottom.insertAdjacentHTML("beforeend", item.outerHTML);
+  });
+  imageWrapperBottomChildren
+    .slice(-columnBottom)
+    .reverse()
+    .forEach((item) => {
+      imageWrapperBottom.insertAdjacentHTML("afterbegin", item.outerHTML);
+    });
+
+  $(".image-wrapper-bottom").addClass("no-smooth");
+  $(".image-wrapper-bottom").scrollLeft(imageWrapperBottom.offsetWidth);
+  $(".image-wrapper-bottom").removeClass("no-smooth");
+
+  //needed for looping
+  $(".image-wrapper-bottom").on("scroll", function () {
+    if (imageWrapperBottom.scrollLeft === 0) {
+      imageWrapperBottom.classList.add("no-smooth");
+      imageWrapperBottom.scrollLeft =
+        imageWrapperBottom.scrollWidth - imageWrapperBottom.offsetWidth;
+      imageWrapperBottom.classList.remove("no-smooth");
+    } else if (
+      imageWrapperBottom.scrollLeft ===
+      imageWrapperBottom.scrollWidth - imageWrapperBottom.offsetWidth
+    ) {
+      imageWrapperBottom.classList.add("no-smooth");
+      imageWrapperBottom.scrollLeft = imageWrapperBottom.offsetWidth;
+      imageWrapperBottom.classList.remove("no-smooth");
+    }
+  });
+  //-------------------------------------------------------
   //ARROW CHANGE ON HOVER AND CLICK
   $(".next-arrow")
-    .on("click", function () {})
+    .on("click", function () {
+      imageWrapper.scrollLeft += widthToScroll;
+      imageWrapperBottom.scrollLeft += widthToScrollBottom;
+    })
     .on("mouseenter", function () {
       $(".next-arrow img").attr("src", "img/arrow-blue-right.png");
     })
@@ -53,7 +88,10 @@ $(document).ready(function () {
     });
 
   $(".prev-arrow")
-    .on("click", function () {})
+    .on("click", function () {
+      imageWrapper.scrollLeft -= widthToScroll;
+      imageWrapperBottom.scrollLeft -= widthToScrollBottom;
+    })
     .on("mouseenter", function () {
       $(".prev-arrow img").attr("src", "img/arrow-blue-left.png");
     })
