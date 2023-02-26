e8acd5ce4f0c330552304e515acb184ec95432ce
lan-e <neladuranec@gmail.com>
2023-02-26 14:04:50 +0100

    simpler JS

--- src/script.js
+++ src/script.js
@@ -3,26 +3,10 @@ $(document).ready(function () {
   let imgWrapperBottom = $(".image-wrapper-bottom");
   let imgTop = ".image-wrapper img";
   let imgBottom = ".image-wrapper-bottom img";
-  //let array = $(imgTop).map(function (index, element) {return '<img src="' + $(element).attr("src") + '" alt="">';}).get();
-  //let arrayBottom = $(imgBottom).map(function (index, element) {return '<img src="' + $(element).attr("src") + '" alt="">';}).get();
-  let i = 5;
-  let j = 4;
   $(".next-arrow")
     .on("click", function () {
-      if (i > 0) {
-        $(imgTop).last().prependTo(imgWrapperTop);
-        i--;
-      } else if (i == 0) {
-        $(imgTop).last().prependTo(imgWrapperTop);
-        i = 5;
-      }
-      if (j > 0) {
-        $(imgBottom).last().prependTo(imgWrapperBottom);
-        j--;
-      } else if (j == 0) {
-        $(imgBottom).last().prependTo(imgWrapperBottom);
-        j = 4;
-      }
+      $(imgTop).last().prependTo(imgWrapperTop);
+      $(imgBottom).last().prependTo(imgWrapperBottom);
     })
     .on("mouseenter", function () {
       $(".next-arrow img").attr("src", "img/arrow-blue-right.png");
@@ -32,20 +16,8 @@ $(document).ready(function () {
     });
   $(".prev-arrow")
     .on("click", function () {
-      if (i > 0) {
-        $(imgTop).first().hide().appendTo(imgWrapperTop).fadeIn("slow");
-        i--;
-      } else if (i == 0) {
-        $(imgTop).first().hide().appendTo(imgWrapperTop).fadeIn("slow");
-        i = 5;
-      }
-      if (j > 0) {
-        $(imgBottom).first().hide().appendTo(imgWrapperBottom).fadeIn("slow");
-        j--;
-      } else if (j == 0) {
-        $(imgBottom).first().hide().appendTo(imgWrapperBottom).fadeIn("slow");
-        j = 4;
-      }
+      $(imgTop).first().hide().appendTo(imgWrapperTop).fadeIn("slow");
+      $(imgBottom).first().hide().appendTo(imgWrapperBottom).fadeIn("slow");
     })
     .on("mouseenter", function () {
       $(".prev-arrow img").attr("src", "img/arrow-blue-left.png");
